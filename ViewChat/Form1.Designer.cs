c100909d72857a963f66e3f9e3b5f28f286a2cbc
arinmk <115914553+arinmk@users.noreply.github.com>
2024-02-12 22:34:05 +0400

    New Logic

--- ViewChat/Form1.Designer.cs
+++ ViewChat/Form1.Designer.cs
@@ -32,17 +32,32 @@
             this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager(this.components);
             this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
             this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.marqueeProgressBarControl1 = new DevExpress.XtraEditors.MarqueeProgressBarControl();
             this.dockPanel2 = new DevExpress.XtraBars.Docking.DockPanel();
             this.dockPanel2_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.memoEdit2 = new DevExpress.XtraEditors.MemoEdit();
             this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
             this.dockPanel1.SuspendLayout();
+            this.dockPanel1_Container.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).BeginInit();
             this.dockPanel2.SuspendLayout();
             this.dockPanel2_Container.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dockManager1
@@ -71,7 +86,7 @@
             this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Left;
             this.dockPanel1.ID = new System.Guid("2729238e-07bb-4307-9dff-059b68e8a9ed");
             this.dockPanel1.Location = new System.Drawing.Point(0, 0);
-            this.dockPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.dockPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.dockPanel1.Name = "dockPanel1";
             this.dockPanel1.Options.AllowDockAsTabbedDocument = false;
             this.dockPanel1.Options.AllowDockBottom = false;
@@ -83,25 +98,58 @@
             this.dockPanel1.Options.ShowCloseButton = false;
             this.dockPanel1.Options.ShowMaximizeButton = false;
             this.dockPanel1.Options.ShowMinimizeButton = false;
-            this.dockPanel1.OriginalSize = new System.Drawing.Size(368, 200);
-            this.dockPanel1.Size = new System.Drawing.Size(368, 1026);
+            this.dockPanel1.OriginalSize = new System.Drawing.Size(163, 200);
+            this.dockPanel1.Size = new System.Drawing.Size(163, 467);
             this.dockPanel1.Text = "User List";
             // 
             // dockPanel1_Container
             // 
-            this.dockPanel1_Container.Location = new System.Drawing.Point(6, 42);
-            this.dockPanel1_Container.Margin = new System.Windows.Forms.Padding(4);
+            this.dockPanel1_Container.Controls.Add(this.gridControl1);
+            this.dockPanel1_Container.Controls.Add(this.marqueeProgressBarControl1);
+            this.dockPanel1_Container.Location = new System.Drawing.Point(3, 21);
+            this.dockPanel1_Container.Margin = new System.Windows.Forms.Padding(2);
             this.dockPanel1_Container.Name = "dockPanel1_Container";
-            this.dockPanel1_Container.Size = new System.Drawing.Size(351, 978);
+            this.dockPanel1_Container.Size = new System.Drawing.Size(156, 443);
             this.dockPanel1_Container.TabIndex = 0;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(4);
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(156, 426);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.DetailHeight = 303;
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            // 
+            // marqueeProgressBarControl1
+            // 
+            this.marqueeProgressBarControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.marqueeProgressBarControl1.EditValue = "Searching...";
+            this.marqueeProgressBarControl1.Location = new System.Drawing.Point(0, 426);
+            this.marqueeProgressBarControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.marqueeProgressBarControl1.Name = "marqueeProgressBarControl1";
+            this.marqueeProgressBarControl1.Properties.ShowTitle = true;
+            this.marqueeProgressBarControl1.Size = new System.Drawing.Size(156, 17);
+            this.marqueeProgressBarControl1.TabIndex = 0;
             // 
             // dockPanel2
             // 
             this.dockPanel2.Controls.Add(this.dockPanel2_Container);
             this.dockPanel2.Dock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
             this.dockPanel2.ID = new System.Guid("7db4e694-8c6d-4772-a4dd-7ebc853b70cb");
-            this.dockPanel2.Location = new System.Drawing.Point(368, 0);
-            this.dockPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.dockPanel2.Location = new System.Drawing.Point(163, 0);
+            this.dockPanel2.Margin = new System.Windows.Forms.Padding(2);
             this.dockPanel2.Name = "dockPanel2";
             this.dockPanel2.Options.AllowDockAsTabbedDocument = false;
             this.dockPanel2.Options.AllowDockBottom = false;
@@ -114,65 +162,130 @@
             this.dockPanel2.Options.ShowCloseButton = false;
             this.dockPanel2.Options.ShowMaximizeButton = false;
             this.dockPanel2.Options.ShowMinimizeButton = false;
-            this.dockPanel2.OriginalSize = new System.Drawing.Size(1165, 200);
-            this.dockPanel2.Size = new System.Drawing.Size(1548, 1026);
+            this.dockPanel2.OriginalSize = new System.Drawing.Size(731, 200);
+            this.dockPanel2.Size = new System.Drawing.Size(735, 467);
             this.dockPanel2.Text = "ChatBody";
             // 
             // dockPanel2_Container
             // 
-            this.dockPanel2_Container.Controls.Add(this.simpleButton1);
-            this.dockPanel2_Container.Controls.Add(this.memoEdit2);
+            this.dockPanel2_Container.Controls.Add(this.groupControl1);
+            this.dockPanel2_Container.Controls.Add(this.simpleButton2);
+            this.dockPanel2_Container.Controls.Add(this.textEdit2);
+            this.dockPanel2_Container.Controls.Add(this.labelControl1);
             this.dockPanel2_Container.Controls.Add(this.memoEdit1);
-            this.dockPanel2_Container.Location = new System.Drawing.Point(6, 42);
-            this.dockPanel2_Container.Margin = new System.Windows.Forms.Padding(4);
+            this.dockPanel2_Container.Location = new System.Drawing.Point(3, 21);
+            this.dockPanel2_Container.Margin = new System.Windows.Forms.Padding(2);
             this.dockPanel2_Container.Name = "dockPanel2_Container";
-            this.dockPanel2_Container.Size = new System.Drawing.Size(1536, 978);
+            this.dockPanel2_Container.Size = new System.Drawing.Size(729, 443);
             this.dockPanel2_Container.TabIndex = 0;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(316, 221);
+            this.simpleButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(93, 21);
+            this.simpleButton2.TabIndex = 3;
+            this.simpleButton2.Text = "Save";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(2, 2);
+            this.textEdit1.Margin = new System.Windows.Forms.Padding(4);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Agave Nerd Font Mono", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.textEdit1.Size = new System.Drawing.Size(656, 20);
+            this.textEdit1.TabIndex = 4;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(316, 193);
+            this.textEdit2.Margin = new System.Windows.Forms.Padding(4);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(93, 20);
+            this.textEdit2.TabIndex = 2;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(316, 173);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(91, 13);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Enter Your Id";
             // 
             // simpleButton1
             // 
-            this.simpleButton1.Location = new System.Drawing.Point(1461, 914);
-            this.simpleButton1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.simpleButton1.Location = new System.Drawing.Point(666, 2);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(4);
             this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 64);
+            this.simpleButton1.Size = new System.Drawing.Size(61, 18);
             this.simpleButton1.TabIndex = 3;
-            this.simpleButton1.Text = ">";
+            this.simpleButton1.Text = "SEND";
             this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
-            // 
-            // memoEdit2
-            // 
-            this.memoEdit2.Location = new System.Drawing.Point(8, 914);
-            this.memoEdit2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.memoEdit2.Name = "memoEdit2";
-            this.memoEdit2.Size = new System.Drawing.Size(1445, 64);
-            this.memoEdit2.TabIndex = 2;
             // 
             // memoEdit1
             // 
-            this.memoEdit1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.memoEdit1.Enabled = false;
+            this.memoEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.memoEdit1.Location = new System.Drawing.Point(0, 0);
-            this.memoEdit1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.memoEdit1.Margin = new System.Windows.Forms.Padding(4);
             this.memoEdit1.Name = "memoEdit1";
-            this.memoEdit1.Size = new System.Drawing.Size(1536, 862);
+            this.memoEdit1.Properties.Appearance.ForeColor = System.Drawing.Color.White;
+            this.memoEdit1.Properties.Appearance.Options.UseForeColor = true;
+            this.memoEdit1.Properties.ReadOnly = true;
+            this.memoEdit1.Properties.UseReadOnlyAppearance = false;
+            this.memoEdit1.Size = new System.Drawing.Size(731, 414);
             this.memoEdit1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.textEdit1);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupControl1.Location = new System.Drawing.Point(0, 421);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.ShowCaption = false;
+            this.groupControl1.Size = new System.Drawing.Size(729, 22);
+            this.groupControl1.TabIndex = 5;
+            this.groupControl1.Text = "Sender";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1916, 1026);
+            this.ClientSize = new System.Drawing.Size(898, 467);
             this.Controls.Add(this.dockPanel2);
             this.Controls.Add(this.dockPanel1);
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
             this.dockPanel1.ResumeLayout(false);
+            this.dockPanel1_Container.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).EndInit();
             this.dockPanel2.ResumeLayout(false);
             this.dockPanel2_Container.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.memoEdit2.Properties)).EndInit();
+            this.dockPanel2_Container.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -185,8 +298,16 @@
         private DevExpress.XtraBars.Docking.DockPanel dockPanel2;
         private DevExpress.XtraBars.Docking.ControlContainer dockPanel2_Container;
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
-        private DevExpress.XtraEditors.MemoEdit memoEdit2;
-        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        public DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.MarqueeProgressBarControl marqueeProgressBarControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
     }
 }
 
--- ViewChat/Form1.cs
+++ ViewChat/Form1.cs
@@ -6,19 +6,129 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices.ComTypes;
+using System.Net.Sockets;
+using System.Net;
 
 namespace ViewChat
 {
     public partial class Form1 : DevExpress.XtraEditors.XtraForm
     {
+        public string Id;
+        public string MSG;
+        private static UdpClient udpClient = new UdpClient();
+        private static readonly int ListenPort = 11000;
+        private static bool isRunning = true;
+        public string YourMSG;
         public Form1()
         {
             InitializeComponent();
+            textEdit1.Visible = false;
+            StartClient();
         }
-        public string YourMSG = "";
-        private void simpleButton1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void UserSideDisplay()
+        {
+            var Mtime = DateTime.Now.ToShortTimeString();
+            memoEdit1.AppendLine($"{Mtime.Replace("PM", "")} {Id.ToUpper()}: {textEdit1.Text}");
+           
+            //memoEdit1.AppendLine($"{Mtime.Replace("PM", "")} {Id.ToUpper()}: {MSG}");
+            textEdit1.Text = "";
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (simpleButton2.Visible)
+            {
+                if (keyData == Keys.Enter)
+                {
+                    idSetter();
+                    return true;
+                }
+            }
+            else
+            {
+                if (keyData == Keys.Enter)
+                {
+                    UserSideDisplay();
+                    return true;
+                }
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+        public  void StartClient()
+        {
+            new Thread(() =>
+            {
+                // Allow sending broadcast messages
+                udpClient.EnableBroadcast = true;
+
+                // Start receiving messages in a separate thread
+                Thread receiveThread = new Thread(ReceiveMessages);
+                receiveThread.Start();
+
+                //Console.WriteLine("Enter messages to send, 'exit' to quit:");
+                while (isRunning)
+                {
+                    string message = YourMSG;
+                    //byte[] bytes = Encoding.ASCII.GetBytes(message);
+                    //udpClient.Send(bytes, bytes.Length, new IPEndPoint(IPAddress.Broadcast, ListenPort));
+                }
+
+                udpClient.Close();
+            })
+            {IsBackground = true }.Start();
+        }
+
+        private  void ReceiveMessages()
+        {
+            new Thread(() =>
+            {
+                udpClient = new UdpClient(ListenPort);
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, ListenPort);
+
+                try
+                {
+                    while (isRunning)
+                    {
+                        byte[] bytes = udpClient.Receive(ref remoteEndPoint);
+                        string message = Encoding.ASCII.GetString(bytes);
+                        MSG = message;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+                finally
+                {
+                    udpClient.Close();
+                }
+            })
+            {IsBackground = true }.Start();
+        }
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            UserSideDisplay();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            idSetter();
+        }
+        private void idSetter()
+        {
+            Id = textEdit2.Text;
+            simpleButton2.Visible = false;
+            textEdit2.Visible = false;
+            labelControl1.Visible = false;
+            textEdit1.Visible = true;
         }
     }
 }
